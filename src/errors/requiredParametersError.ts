2bdc5ebfff1153ddf46317a369974e7099a5bfe1
João Martins <jvmartinsdev@gmail.com>
2022-11-14 19:16:39 -0300

    feat: add either error handling

---
+++ src/errors/requiredParametersError.ts
@@ -0,0 +1,10 @@
+export class RequiredParametersError extends Error {
+  public _message: string
+  public _statusCode: number
+
+  constructor (message: string, statusCode = 500) {
+    super(message)
+    this._message = message
+    this._statusCode = statusCode
+  }
+}
